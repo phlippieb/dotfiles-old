c28a7dfa8f1fe18323537b0d0ba96b8e458cf5c1
phlippieb <phlippie.bosman@gmail.com>
2018-03-27 18:50:13 +0200

    Update output

--- git/install.sh
+++ git/install.sh
@@ -1,29 +1,26 @@
 # List of dotfiles to install
 files="gitconfig gitignore_global git-completion.bash"
 
-echo "==> Installing git dotfiles..."
+echo "installing $(tput setaf 4 && tput bold)git$(tput sgr0) dotfiles..."
 
 # Install each dotfile
 for file in $files; do
-    echo "==> Installing .$file..."
+    echo " - installing $(tput setaf 4).$file$(tput sgr0)..."
     
     # If the dotfile exists, back it up
     if [ -f ~/.$file ]; then
         backup_file=".$file.$(date +'%Y%m%d%H%M%S')"
-        echo "--> Existing dotfile found. Backing up to $backup_file..."
+        echo " - $(tput setaf 3)existing file found.$(tput sgr0)"
+        echo "   backing up to $backup_file"
         # TODO: 
         # cp ~/.$file ~/$backup_file
-        echo "--> Done."
-        echo "--> Removing existing dotfile..."
         # TODO:
         # rm ~/.$file
-        echo "--> Done."
     fi
     
     # Install the new dotfile by symlinking here
     # TODO:
     # ln -s ./$file ~/.$file
-    echo "==> Done."
+    echo " - $(tput setaf 2)done.$(tput sgr0)"
 done
-
-echo "==> Git dotfiles installed."
+echo
--- tmux/install.sh
+++ tmux/install.sh
@@ -1,29 +1,26 @@
 # List of dotfiles to install
 files="tmux.conf tmux.light-theme.conf"
 
-echo "==> Installing tmux dotfiles..."
+echo "installing $(tput setaf 4 && tput bold)tmux$(tput sgr0) dotfiles..."
 
 # Install each dotfile
 for file in $files; do
-    echo "==> Installing .$file..."
+    echo " - installing $(tput setaf 4).$file$(tput sgr0)..."
     
     # If the dotfile exists, back it up
     if [ -f ~/.$file ]; then
         backup_file=".$file.$(date +'%Y%m%d%H%M%S')"
-        echo "--> Existing dotfile found. Backing up to $backup_file..."
+        echo " - $(tput setaf 3)existing file found.$(tput sgr0)"
+        echo "   backing up to $backup_file"
         # TODO: 
         # cp ~/.$file ~/$backup_file
-        echo "--> Done."
-        echo "--> Removing existing dotfile..."
         # TODO:
         # rm ~/.$file
-        echo "--> Done."
     fi
     
     # Install the new dotfile by symlinking here
     # TODO:
     # ln -s ./$file ~/.$file
-    echo "==> Done."
+    echo " - $(tput setaf 2)done.$(tput sgr0)"
 done
-
-echo "==> tmux dotfiles installed."
+echo
